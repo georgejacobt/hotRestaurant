d6150f58e99421dbcf9356620cbb43959d0e1bec
sturcios <sen.turcios@gmail.com>
2018-07-28 13:32:31 -0500

    added api/backend

--- server.js
+++ server.js
@@ -13,12 +13,61 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-    // res.sendFile(path.join(__dirname, "index.html"));
+// Reservation to start 
 
+var reservations = [
+  {
+ 
+    name: "Sendy",
+    phonenumber: 7777777777,
+    email: "sendy@sendy.com",
+    unique_id: "table1"
+  },
+ 
+ 
+ ];
+
+
+app.get("/", function(req, res) {
+  
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
+  app.get("/reservation.html", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "reservation.html"));
+   });
+   
+   app.get("/tables.html", function (req, res) {
+   
+    res.sendFile(path.join(__dirname, "tables.html"));
+   });
+
+
+
+  
+  //Using GET to retrieve all reservation information 
+  
+  app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+  });
+  
+  
+  // Create New Reservation - takes in JSON input
+  app.post("/api/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newReservation = req.body;
+  
+    console.log(newReservation);
+  
+    // We then add the json the user sent to the character array
+    reservations.push(newReservation);
+  
+    // We then display the JSON to the users
+    res.json(newReservation);
+  });
+  
 
 
 
